fe192da5b380da71ec85c42fe2738217f4bddf87
arm64v9a <153753809+arm64v9a@users.noreply.github.com>
2026-02-25 11:45:33 +0300

    inital commit

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "flake for nekomesh";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?depth=1";
+
+  outputs =
+    { self, ... }@inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin" # idk about darwin, aarch64 not tested
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              clang-tools
+              platformio
+              esptool
+              tio
+              git
+            ];
+            shellHook = ''
+              export PLATFORMIO_CORE_DIR=$PWD/.platformio
+            '';
+          };
+        }
+      );
+      formatter = forEachSupportedSystem (
+        { pkgs }:
+        pkgs.writeShellApplication {
+          name = "fmt";
+          runtimeInputs = with pkgs; [
+            clang-tools
+            fd
+          ];
+
+          text = ''
+            fd -e h -e cpp -e c -x clang-format -i
+          '';
+        }
+      );
+    };
+}
